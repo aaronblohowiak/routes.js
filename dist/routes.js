39877d86337132f11e88b854b1480fe3ea1ed3c7
Blaine Bublitz <blaine@iceddev.com>
2014-02-25 18:57:12 -0700

    Generate the UMD wrapped file and check it in

---
+++ dist/routes.js
@@ -0,0 +1,153 @@
+!function(e){if("object"==typeof exports)module.exports=e();else if("function"==typeof define&&define.amd)define(e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.routes=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
+
+var localRoutes = [];
+
+
+/**
+ * Convert path to route object
+ *
+ * A string or RegExp should be passed,
+ * will return { re, src, keys} obj
+ *
+ * @param  {String / RegExp} path
+ * @return {Object}
+ */
+ 
+var Route = function(path){
+  //using 'new' is optional
+  
+  var src, re, keys = [];
+  
+  if(path instanceof RegExp){
+    re = path;
+    src = path.toString();
+  }else{
+    re = pathToRegExp(path, keys);
+    src = path;
+  }
+
+  return {
+  	 re: re,
+  	 src: path.toString(),
+  	 keys: keys
+  }
+};
+
+/**
+ * Normalize the given path string,
+ * returning a regular expression.
+ *
+ * An empty array should be passed,
+ * which will contain the placeholder
+ * key names. For example "/user/:id" will
+ * then contain ["id"].
+ *
+ * @param  {String} path
+ * @param  {Array} keys
+ * @return {RegExp}
+ */
+var pathToRegExp = function (path, keys) {
+	path = path
+		.concat('/?')
+		.replace(/\/\(/g, '(?:/')
+		.replace(/(\/)?(\.)?:(\w+)(?:(\(.*?\)))?(\?)?/g, function(_, slash, format, key, capture, optional){
+			keys.push(key);
+			slash = slash || '';
+			return ''
+				+ (optional ? '' : slash)
+				+ '(?:'
+				+ (optional ? slash : '')
+				+ (format || '') + (capture || '([^/]+?)') + ')'
+				+ (optional || '');
+		})
+		.replace(/([\/.])/g, '\\$1')
+		.replace(/\*/g, '(.+)');
+	return new RegExp('^' + path + '$', 'i');
+};
+
+/**
+ * Attempt to match the given request to
+ * one of the routes. When successful
+ * a  {fn, params, splats} obj is returned
+ *
+ * @param  {Array} routes
+ * @param  {String} uri
+ * @return {Object}
+ */
+var match = function (routes, uri) {
+	var captures, i = 0;
+
+	for (var len = routes.length; i < len; ++i) {
+		var route = routes[i],
+		    re = route.re,
+		    keys = route.keys,
+		    splats = [],
+		    params = {};
+
+		if (captures = re.exec(uri)) {
+			for (var j = 1, len = captures.length; j < len; ++j) {
+				var key = keys[j-1],
+					val = typeof captures[j] === 'string'
+						? decodeURIComponent(captures[j])
+						: captures[j];
+				if (key) {
+					params[key] = val;
+				} else {
+					splats.push(val);
+				}
+			}
+			return {
+				params: params,
+				splats: splats,
+				route: route.src
+			};
+		}
+	}
+};
+
+/**
+ * Default "normal" router constructor.
+ * accepts path, fn tuples via addRoute
+ * returns {fn, params, splats, route}
+ *  via match
+ *
+ * @return {Object}
+ */
+ 
+var Router = function(){
+  //using 'new' is optional
+  return {
+    routes: [],
+    routeMap : {},
+    addRoute: function(path, fn){
+      if (!path) throw new Error(' route requires a path');
+      if (!fn) throw new Error(' route ' + path.toString() + ' requires a callback');
+
+      var route = Route(path);
+      route.fn = fn;
+
+      this.routes.push(route);
+      this.routeMap[path] = fn;
+    },
+
+    match: function(pathname){
+      var route = match(this.routes, pathname);
+      if(route){
+        route.fn = this.routeMap[route.route];
+      }
+      return route;
+    }
+  }
+};
+
+Router.Route = Route
+Router.pathToRegExp = pathToRegExp
+Router.match = match
+// back compat
+Router.Router = Router
+
+module.exports = Router
+
+},{}]},{},[1])
+(1)
+});
